daf6e4c1048d78bfc656d83404822087e62e1a83
ap9827388-coder <ap9827388@gmail.com>
2026-02-16 19:26:36 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    void Start()
+    {
+        //вывод покармить кошку перед шклолй
+        print("HelloWorld");
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+}   
